5aaabd429319d7ba32e4d83e1fc1e855b94ffc14
Maris Lagzdins (TestDevLab) <maaris.lagzdins+developer@gmail.com>
2019-11-08 11:39:52 +0200

    Finished Project 28

---
+++ Project28/Project28/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project28
+//
+//  Created by Maris Lagzdins on 08/11/2019.
+//  Copyright © 2019 Developer Maris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
